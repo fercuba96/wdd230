ea8d955c0e0ea3386fcb69efce28ed7ddbe8c4a9
fercuba96 <fercuba96@gmail.com>
2024-04-29 10:32:13 -0500

    update please

---
+++ scripts/getDates.js
@@ -0,0 +1,15 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const currentYear = new Date().getFullYear();
+
+    const copyrightYear = document.getElementById("copyright");
+    if (copyrightYear) {
+        copyrightYear.textContent = "© " + currentYear;
+    }
+
+    const lastModified = document.lastModified;
+
+    const lastModifiedParagraph = document.getElementById("lastModified");
+    if (lastModifiedParagraph) {
+        lastModifiedParagraph.textContent = "Last modified: " + lastModified;
+    }
+});
