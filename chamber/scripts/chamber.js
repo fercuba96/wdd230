c565c73ef5b62b7906dbcdbc65ff185d52c50159
fercuba96 <fercuba96@gmail.com>
2024-05-04 09:07:28 -0500

    Chamber plan done

---
+++ chamber/scripts/chamber.js
@@ -0,0 +1,27 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const currentYear = new Date().getFullYear();
+
+    const copyrightYear = document.getElementById("copyright");
+    if (copyrightYear) {
+        copyrightYear.textContent = "© " + currentYear;
+    }
+
+    const lastModified = document.lastModified;
+
+    const lastModifiedParagraph = document.getElementById("lastModified");
+    if (lastModifiedParagraph) {
+        lastModifiedParagraph.textContent = "Last modified: " + lastModified;
+    }
+});
+
+document.getElementById("projectinfo").textContent = "WDD230 Course project";
+
+
+const hamButton = document.querySelector('#menu');
+const navigation = document.querySelector('.navigation');
+
+hamButton.addEventListener('click', () => {
+    navigation.classList.toggle('open');
+    hamButton.classList.toggle('open');
+});
+
