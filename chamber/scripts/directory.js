de7ce38c30ee470186cea252f012309bbd82bc28
fercuba96 <fercuba96@gmail.com>
2024-05-26 00:06:07 -0500

    step by step

--- chamber/scripts/directory.js
+++ chamber/scripts/directory.js
@@ -8,29 +8,29 @@ async function getDirectoryData() {
 
 const displayCompanies = (per_companies) => {
     per_companies.forEach((per_company) => {
-        let card = document.createElement('section');
-        let fullName = document.createElement('h2'); // fill in the blank
+        let companyinfo = document.createElement('section');
+        let compName = document.createElement('h2');
         let portrait = document.createElement('img');
         let dateofbirth = document.createElement('p');
         let placeofbirth = document.createElement('ul');
 
-        fullName.textContent = `${per_company.name} ${per_company.lastname}`;
+        compName.textContent = `${per_company.name}`;
         dateofbirth.textContent = `Date of birth: ${per_company.birthdate}`;
         placeofbirth.textContent = `Place of birth: ${per_company.birthplace}`;
 
-        portrait.setAttribute('src', per_company.imageurl);
-        portrait.setAttribute('alt', `Portrait of ${per_company.name} ${per_company.lastname}`); // fill in the blank
+        portrait.setAttribute('src', per_company.image);
+        portrait.setAttribute('alt', `Portrait of ${per_company.name}`);
         portrait.setAttribute('loading', 'lazy');
         portrait.setAttribute('width', '340');
         portrait.setAttribute('height', '440');
 
-        card.appendChild(fullName);
-        card.appendChild(portrait);
-        card.appendChild(dateofbirth);
-        card.appendChild(placeofbirth);
+        companyinfo.appendChild(compName);
+        companyinfo.appendChild(portrait);
+        companyinfo.appendChild(dateofbirth);
+        companyinfo.appendChild(placeofbirth);
 
 
-        companies.appendChild(card);
+        companies.appendChild(companyinfo);
     });
 }
 
