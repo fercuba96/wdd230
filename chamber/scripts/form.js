2665f5c6bab97a3ee71d8fb535f837688194011f
fercuba96 <fercuba96@gmail.com>
2024-06-03 12:09:26 -0500

    info placed

--- chamber/scripts/form.js
+++ chamber/scripts/form.js
@@ -12,16 +12,35 @@ document.addEventListener("DOMContentLoaded", function () {
     if (lastModifiedParagraph) {
         lastModifiedParagraph.textContent = "Last modified: " + lastModified;
     }
+
+    document.getElementById("projectinfo").textContent = "WDD230 Course project";
+
+    document.getElementById("contactinfo").textContent = "Chamber:  +51 934 163 838 / fercuba96@gmail.com";
+
+    const hamButton = document.querySelector('#menu');
+    const navigation = document.querySelector('.navigation');
+
+    hamButton.addEventListener('click', () => {
+        navigation.classList.toggle('open');
+        hamButton.classList.toggle('open');
+    });
+
+    const joinForm = document.getElementById('joinChamber');
+    const submissionTimeDiv = document.getElementById('timestamp');
+
+    joinForm.addEventListener('submit', function (event) {
+        if (joinForm.checkValidity()) {
+
+            const now = new Date();
+            const dateTimeString = now.toLocaleString();
+
+
+            submissionTimeDiv.textContent = `Form submitted on: ${dateTimeString}`;
+        } else {
+            event.preventDefault(); // Prevent submission if form is not valid
+
+        }
+    });
+
 });
 
-document.getElementById("projectinfo").textContent = "WDD230 Course project";
-
-document.getElementById("contactinfo").textContent = "Chamber:  +51 934 163 838 / fercuba96@gmail.com";
-
-const hamButton = document.querySelector('#menu');
-const navigation = document.querySelector('.navigation');
-
-hamButton.addEventListener('click', () => {
-    navigation.classList.toggle('open');
-    hamButton.classList.toggle('open');
-});
